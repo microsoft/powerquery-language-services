4d2f3d373a9c8bff5667ff3033036db232f487ce
Matt Masson <mmasson@microsoft.com>
2020-06-01 14:01:42 -0400

    add external test scenarios

--- src/language-services/commonTypes.ts
+++ src/language-services/commonTypes.ts
@@ -2,11 +2,29 @@
 // Licensed under the MIT license.
 
 import type { DocumentUri, TextDocument } from "vscode-languageserver-textdocument";
-import type { Diagnostic, Position, Range } from "vscode-languageserver-types";
-import { DiagnosticSeverity } from "vscode-languageserver-types";
+import type {
+    CompletionItem,
+    Diagnostic,
+    DocumentSymbol,
+    Hover,
+    Position,
+    Range,
+    SignatureHelp,
+} from "vscode-languageserver-types";
+import { CompletionItemKind, DiagnosticSeverity, SymbolKind } from "vscode-languageserver-types";
 
-export type { Diagnostic, DocumentUri, Position, Range, TextDocument };
-export { DiagnosticSeverity };
+export type {
+    CompletionItem,
+    Diagnostic,
+    DocumentSymbol,
+    DocumentUri,
+    Hover,
+    Position,
+    Range,
+    SignatureHelp,
+    TextDocument,
+};
+export { CompletionItemKind, DiagnosticSeverity, SymbolKind };
 
 export interface IDisposable {
     dispose(): void;
---
+++ src/test/language-services/externalConsumption.ts
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// tslint:disable: no-implicit-dependencies
+import { expect } from "chai";
+import "mocha";
+
+// We should only import from the library index
+import * as LanguageServices from "../../language-services";
+import {
+    Analysis,
+    AnalysisOptions,
+    CompletionItemKind,
+    DiagnosticSeverity,
+    Hover,
+    Position,
+    SymbolKind,
+    TextDocument,
+} from "../../language-services";
+
+describe("External consumption", () => {
+    it("Analysis", async () => {
+        const options: AnalysisOptions = {
+            locale: "en-us",
+            maintainWorkspaceCache: false,
+        };
+
+        const textDocument: TextDocument = LanguageServices.createTextDocument("id", 1, "let a = 1 in a");
+
+        const position: Position = {
+            character: 1,
+            line: 0,
+        };
+
+        const analysis: Analysis = LanguageServices.createAnalysisSession(textDocument, position, options);
+        const hover: Hover = await analysis.getHover();
+
+        expect(hover.range === undefined);
+        expect(hover.contents === null);
+
+        analysis.dispose();
+    });
+
+    it("CompletionItemKind", () => {
+        expect(CompletionItemKind.Text).to.equal(1);
+        expect(CompletionItemKind.Keyword).to.equal(14);
+    });
+
+    it("DiagnosticSeverity", () => {
+        expect(DiagnosticSeverity.Error).to.equal(1);
+        expect(DiagnosticSeverity.Warning).to.equal(2);
+        expect(DiagnosticSeverity.Information).to.equal(3);
+        expect(DiagnosticSeverity.Hint).to.equal(4);
+    });
+
+    it("SymbolKind", () => {
+        expect(SymbolKind.Function).to.equal(12);
+        expect(SymbolKind.Constant).to.equal(14);
+    });
+});
