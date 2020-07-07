742666fcc6791ddca6da3182e63dbc7e3f778ea1
Matt Masson <mmasson@microsoft.com>
2020-07-07 13:14:45 -0400

    diagnostic error codes

---
+++ src/language-services/diagnosticErrorCode.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export enum DiagnosticErrorCode {
+    DuplicateIdentifier = "Error.DuplicateIndentifier",
+    ParserError = "Error.Parser",
+    LexError = "Error.Lex",
+}
