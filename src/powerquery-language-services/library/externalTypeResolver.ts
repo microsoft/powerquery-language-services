252f66b58b59bb0f8027d7a281ace8a53d122583
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-02-03 12:48:00 -0800

    impl for resolveTableAddColumn

--- src/powerquery-language-services/library/externalTypeResolver.ts
+++ src/powerquery-language-services/library/externalTypeResolver.ts
@@ -24,7 +24,8 @@ export function externalTypeResolverFnFactory(
 
         switch (request.kind) {
             case PQP.Language.ExternalType.ExternalTypeRequestKind.Invocation:
-                return externalTypeTrio.kind === ExternalTypeTrioKind.Invocation
+                return externalTypeTrio.kind === ExternalTypeTrioKind.Invocation &&
+                    PQP.Language.TypeUtils.isValidInvocation(externalTypeTrio.value, request.args)
                     ? externalTypeTrio.invocationResolverFn(request)
                     : undefined;
 
@@ -41,7 +42,7 @@ function createLibraryMap(library: Library.Library): LibraryMap {
     const result: Map<string, TExternalTypeTrio> = new Map();
 
     for (const exteranlTypeTrio of ExternalTypeTrios) {
-        if (isExternalTypeTrioIncluded(library, exteranlTypeTrio)) {
+        if (isExternalTypeTrioInLibrary(library, exteranlTypeTrio)) {
             result.set(exteranlTypeTrio.identifierLiteral, exteranlTypeTrio);
         }
     }
@@ -49,14 +50,57 @@ function createLibraryMap(library: Library.Library): LibraryMap {
     return result;
 }
 
-function isExternalTypeTrioIncluded(library: Library.Library, externalTypeTrio: TExternalTypeTrio): boolean {
+function isExternalTypeTrioInLibrary(library: Library.Library, externalTypeTrio: TExternalTypeTrio): boolean {
     return library.has(externalTypeTrio.identifierLiteral);
 }
 
 function resolveTableAddColumn(
-    _request: PQP.Language.ExternalType.ExternalInvocationTypeRequest,
-): PQP.Language.Type.TType | undefined {
-    return undefined;
+    request: PQP.Language.ExternalType.ExternalInvocationTypeRequest,
+): PQP.Language.Type.TTable | PQP.Language.Type.None | undefined {
+    const table: PQP.Language.Type.TType = PQP.Language.TypeUtils.assertAsTable(PQP.Assert.asDefined(request.args[0]));
+    const columnName: PQP.Language.Type.TText = PQP.Language.TypeUtils.assertAsText(
+        PQP.Assert.asDefined(request.args[1]),
+    );
+    const columnGenerator: PQP.Language.Type.TFunction = PQP.Language.TypeUtils.assertAsFunction(
+        PQP.Assert.asDefined(request.args[2]),
+    );
+    const maybeColumnType: PQP.Language.Type.TType | undefined =
+        request.args.length === 4
+            ? PQP.Language.TypeUtils.assertAsType(PQP.Assert.asDefined(request.args[3]))
+            : undefined;
+
+    // We can't mutate the given table without being able to resolve columnName to a literal.
+    if (!PQP.Language.TypeUtils.isTextLiteral(columnName)) {
+        return undefined;
+    }
+
+    let columnType: PQP.Language.Type.TType;
+    if (maybeColumnType !== undefined) {
+        columnType = maybeColumnType;
+    } else if (PQP.Language.TypeUtils.isDefinedFunction(columnGenerator)) {
+        columnType = columnGenerator.returnType;
+    } else {
+        columnType = PQP.Language.Type.AnyInstance;
+    }
+
+    if (PQP.Language.TypeUtils.isDefinedTable(table)) {
+        // We can't overwrite an existing key.
+        if (table.fields.has(PQP.StringUtils.normalizeIdentifier(columnName.literal))) {
+            return PQP.Language.Type.NoneInstance;
+        }
+
+        return PQP.Language.TypeUtils.definedTableFactory(
+            table.isNullable,
+            new Map<string, PQP.Language.Type.TType>([...table.fields.entries(), [columnName.literal, columnType]]),
+            table.isOpen,
+        );
+    } else {
+        return PQP.Language.TypeUtils.definedTableFactory(
+            table.isNullable,
+            new Map<string, PQP.Language.Type.TType>([[columnName.literal, columnType]]),
+            true,
+        );
+    }
 }
 
 type LibraryMap = Map<string, TExternalTypeTrio>;
@@ -76,6 +120,7 @@ interface IExternalTypeTrio {
 
 interface ExteranlInvocationTrio extends IExternalTypeTrio {
     readonly kind: ExternalTypeTrioKind.Invocation;
+    readonly value: PQP.Language.Type.DefinedFunction;
     readonly invocationResolverFn: (
         request: PQP.Language.ExternalType.ExternalInvocationTypeRequest,
     ) => PQP.Language.Type.TType | undefined;
