7bc020feaa7e9bd6ed46f83e3714114bb6cada0a
JordanBoltonMN <jordanboltonmn@gmail.com>
2022-02-14 10:49:54 -0600

    additional comments

--- src/powerquery-language-services/inspectionSettings.ts
+++ src/powerquery-language-services/inspectionSettings.ts
@@ -7,6 +7,16 @@ import { ExternalType } from "./inspection/externalType";
 import { TypeById } from "./inspection";
 
 export interface InspectionSettings extends PQP.Settings {
+    // A goal is to enable smart type resolvers for library functions.
+    // Take for example `Table.AddColumn(tbl, "nameTwice", each [name] + [name])`
+    // There's no way (in the general sense) that you're applying the lambda to the table input.
+    // More specifically, it doesn't know the scope of `_` in the EachExpression.
+    // I can't think of how to solve this problem in the generalized case
+    // without it taking multiple passes and becoming just shy of an evaluator.
+    //
+    // This is a "simple" hack that enables consumers of language-services to enable those smart type resolvers.
+    // An initial pass can be made on InvokeExpressions where it sets the scope for an EachExpression.
     readonly maybeEachScopeById: TypeById | undefined;
+    // Read `externalType.ts` for comments.
     readonly maybeExternalTypeResolver: ExternalType.TExternalTypeResolverFn | undefined;
 }
