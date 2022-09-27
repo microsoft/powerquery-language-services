032d8d7738533495c5a4fe61f35be9eac2eb1b80
JordanBoltonMN <jordanboltonmn@gmail.com>
2022-09-27 09:33:38 -0500

    initial commit

--- src/powerquery-language-services/formatter.ts
+++ src/powerquery-language-services/formatter.ts
@@ -7,25 +7,30 @@ import { Range, TextEdit } from "vscode-languageserver-types";
 import { ResultUtils } from "@microsoft/powerquery-parser";
 import { TextDocument } from "vscode-languageserver-textdocument";
 
-export async function tryFormat(document: TextDocument, settings: PQF.FormatSettings): Promise<TextEdit[]> {
-    const formatSettings: PQF.FormatSettings = {
-        ...PQF.DefaultSettings,
-        ...settings,
-    };
+export function tryFormat(
+    document: TextDocument,
+    settings: PQF.FormatSettings,
+): Promise<PQP.Result<TextEdit[] | undefined, PQP.CommonError.CommonError>> {
+    return ResultUtils.ensureResultAsync(async () => {
+        const formatSettings: PQF.FormatSettings = {
+            ...PQF.DefaultSettings,
+            ...settings,
+        };
 
-    const triedFormat: PQF.TriedFormat = await PQF.tryFormat(formatSettings, document.getText());
+        const triedFormat: PQF.TriedFormat = await PQF.tryFormat(formatSettings, document.getText());
 
-    if (ResultUtils.isOk(triedFormat)) {
-        return [TextEdit.replace(fullDocumentRange(document), triedFormat.value)];
-    }
-    // If an unhandled exception was returned.
-    else if (PQP.CommonError.isCommonError(triedFormat.error)) {
-        throw triedFormat.error;
-    }
-    // Else a lexer or parser error was returned and should be ignored.
-    else {
-        return [];
-    }
+        if (ResultUtils.isOk(triedFormat)) {
+            return [TextEdit.replace(fullDocumentRange(document), triedFormat.value)];
+        }
+        // If an unhandled exception was returned.
+        else if (PQP.CommonError.isCommonError(triedFormat.error)) {
+            throw triedFormat;
+        }
+        // Else a lexer or parser error was returned and should be ignored.
+        else {
+            return undefined;
+        }
+    }, settings.locale);
 }
 
 // TODO: is there a better way to do this?
